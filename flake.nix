7e054322bcbb9a0c6f5c9b3112aff9f4a0b65cb7
chachacollins <collinschacha@hotmail.com>
2025-03-11 14:00:47 +0300

    everything everywhere all at once

--- flake.nix
+++ flake.nix
@@ -3,19 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
   };
   outputs =
     { nixpkgs, ... } @ inputs:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      home-manager = inputs.home-manager;
     in
     {
       nixosConfigurations.witchdoctor = nixpkgs.lib.nixosSystem
         {
           modules = [
             ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.backupFileExtension = "HMBackup";
+              home-manager.useUserPackages = true;
+              home-manager.users.alchemist.imports = [
+                ./home.nix
+              ];
+              home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
+            }
             ({ pkgs, ... }: {
               programs.vim.defaultEditor = true;
             })
