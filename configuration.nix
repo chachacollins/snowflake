58e82fff4162cee3ae9f60c1e7eceaeb6a4efc45
chachacollins <collinschacha@hotmail.com>
2025-03-15 10:34:39 +0300

    everything everywhere all at once

--- configuration.nix
+++ configuration.nix
@@ -94,6 +94,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
     wget
     i3
     dmenu
