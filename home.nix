0a982b56dbc7a6cfafacaa55be5d8c58da82eff0
chachacollins <collinschacha@hotmail.com>
2025-09-28 20:40:47 +0300

    NixOS system update: 420102ef0f660d9b052ba586058034a24dd257f744cbbb1282eb6da9cbea8cc8

--- home.nix
+++ home.nix
@@ -17,9 +17,6 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = with pkgs; [
-    inputs.zen-browser.packages."${system}".default
-  ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
