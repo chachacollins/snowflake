2ed68901444378fb015c0a9788296b2e335c5228
chachacollins <collinschacha@hotmail.com>
2025-10-04 05:09:30 +0300

    NixOS system update: e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   description = "A banger snow flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
@@ -9,31 +8,31 @@
     };
   };
   outputs =
-    { nixpkgs, ... } @ inputs:
+    { self, nixpkgs, ... } @ inputs:  # Added 'self' here
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
       home-manager = inputs.home-manager;
     in
-    {
-      nixosConfigurations.coven= nixpkgs.lib.nixosSystem
-        {
-          modules = [
-            ./configuration.nix
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.backupFileExtension = "backup";
-              home-manager.useUserPackages = true;
-              home-manager.users.alchemist.imports = [
-                ./home.nix
-              ];
-              home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
-            }
-            ({ pkgs, ... }: {
-              programs.vim.defaultEditor = true;
-            })
-          ];
-        };
-
+      {
+      nixosConfigurations.coven = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs self; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.useUserPackages = true;
+            home-manager.users.alchemist.imports = [
+              ./home.nix
+            ];
+            home-manager.extraSpecialArgs = { inherit inputs; system = "x86_64-linux"; };
+          }
+          ({ pkgs, ... }: {
+            programs.vim.defaultEditor = true;
+          })
+        ];
+      };
     };
 }
