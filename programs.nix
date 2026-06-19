868cdf2d9d301c3b320f7d193371b0f89e57b616
chachacollins <collinschacha@hotmail.com>
2026-06-19 21:04:31 +0300

    Add PostgreSQL 16 database service

--- programs.nix
+++ programs.nix
@@ -17,6 +17,17 @@
     package = pkgs.mariadb;
   };
 
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    enableTCPIP = true;
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host  all all 127.0.0.1/32 trust
+      host  all all ::1/128 trust
+    '';
+  };
+
   services.syncthing = {
     enable = true;
     user = "kynikoi";
